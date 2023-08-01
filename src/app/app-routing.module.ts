7993bd941f14af3b0f617a81ff3dcdabd76032c7
guymn <gjp2267@gmail.com>
2023-08-01 14:47:34 +0700

    add router learn and about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LearnComponent } from './learn/learn.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: QuizComponent },
+  { path: 'learn', component: LearnComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
